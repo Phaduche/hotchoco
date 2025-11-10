b71cbdfca0aeeb414691e2ebdabde9664edd8dab
신민기 <sinmingi@sinmingiui-noteubug.local>
2025-11-10 21:05:44 +0900

    코멘트대로 업뎃

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,7 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { redirect } from "react-router";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+export const loader: Route.Loader = () => redirect("/profile");
 
-export default function Home() {
-  return <Welcome />;
+export default function Home() { return null;
 }
